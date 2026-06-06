9019fdcd7d64ffda858959bb3857350ebaae5cea
phelia224-coder <phelia224@gmail.com>
2026-06-06 03:16:29 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const hamburger = document.getElementById("hamburger");
+const mobileNav = document.getElementById("mobileNav");
+const closeBtn = document.getElementById("closeBtn");
+const overlay = document.getElementById("overlay");
+
+hamburger.addEventListener("click", () => {
+    mobileNav.classList.add("active");
+    overlay.classList.add("active");
+});
+
+closeBtn.addEventListener("click", closeMenu);
+overlay.addEventListener("click", closeMenu);
+
+function closeMenu(){
+    mobileNav.classList.remove("active");
+    overlay.classList.remove("active");
+}
